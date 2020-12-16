2c4c48dc4d3e77a5e163324af12511f0863c6f36
venkatesh-golla <golla.venkatesh1997@gmail.com>
2020-12-16 17:32:12 -0500

    Initial Commit with setup and components created

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../Shared/ingredient.model'
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients:Ingredient[]=[
+    new Ingredient('Cheese Bars',2),
+    new Ingredient('Sugar',5)
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
